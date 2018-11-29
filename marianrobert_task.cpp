c258aa1c455f6ad4a929542f9911254e532612fe
marianrobert7 <44975343+marianrobert7@users.noreply.github.com>
2018-11-29 16:48:49 +0200

    Add files via upload

---
+++ marianrobert_task.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    int x;
+    x=1
+    if(n>=0) (return 0)
+        else {do (n=n>>1);}
+        x=x+1
+        while (n>=0)
+
+}
+
+cout<<x;
